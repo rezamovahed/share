a3001625983010606ca23761971c4673a10a3b86
Nathan Henniges <demonwolf@demonwolfdev.com>
2019-03-23 18:33:20 -0500

    Signup route works Just needs verify email.

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -60,25 +60,129 @@ router.get("/signup", (req, res) => {
 */
 router.post("/signup", (req, res) => {
   let error = {};
+  let success = 'Your account has been created but must be activated.  Please check your email.'
+  const username = req.body.username.toLowerCase();
+  const email = req.body.email.toLowerCase();
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
   const avatar = gravatar.url(req.body.email, {
     s: '100',
     r: 'x',
     d: 'retro'
   }, true);
-  const password = req.body.password;
-  const comfirmPassword = req.body.comfirmPassword;
-  const username = req.body.username.toLowerCase();
-  const email = req.body.email.toLowerCase();
-  let newUser = {
-    username,
-    email,
-    avatar,
+
+  // Check if empty
+  // Username
+  if (!username) { error.username = 'Please enter your username.' }
+  // Email
+  if (!email) { error.email = 'Please enter your email.' }
+  // Password
+  if (!password) { error.password = 'Must have a pssword' }
+  if (!confirmPassword) { error.confirmPassword = 'Must comfirm pssword' }
+
+  // Check if email is vaid
+  if (!validator.isEmail(email)) { error.email = 'Email must be vaild (Example someone@example.com)' }
+  // Check if passoword and comfirm password are the same.
+  // Check password length
+  if (!validator.isLength(password, {
+    minimum: 8
+  })) {
+    error.password = 'Password must be at least 8 characters long. '
   }
-  User.register(newUser,password, (err, user) => {
-    console.log(err)
-    console.log(user)
-  });
-  res.send('Account created please go to your email to activate your account.')
+  if (password !== confirmPassword) { error.confirmPassword = 'Both passowrds must match.' }
+
+  if (error === {}) {
+    let newUser = {
+      username,
+      email,
+      avatar,
+    }
+    User.register(newUser, password, (err, user) => {
+      switch (err.name) {
+        case ('UserExistsError'):
+          error.alreadyAccount = 'A user with the given username is already registered'
+          break;
+      }
+      if (JSON.stringify(error) !== '{}') {
+        req.flash("error", error)
+        res.redirect('/signup');
+        return;
+      }
+      async.waterfall([
+        function (done) {
+          crypto.randomBytes(16, function (err, buf) {
+            var token = buf.toString('hex');
+            var tokenExpire = Date.now() + 3600000;
+            done(err, token, tokenExpire)
+          });
+        },
+        function (token, tokenExpire, done) {
+          User.findOne({
+            email: req.body.email
+          }, function (err, user) {
+            user.accountActvationToken = token;
+            user.accountActvationExpire = tokenExpire;
+            user.save(function (err) {
+              done(err, token);
+            });
+          });
+        },
+        function (token, done) {
+          const htmlOuput = mjml(`<mjml>
+        <mj-body background-color="#ffffff" font-size="13px">
+          <mj-section>
+            <mj-column>
+              <mj-text font-style="bold" font-size="24px" color="#626262" align="center">
+                Your account details
+              </mj-text>
+            </mj-column>
+          </mj-section>
+          <mj-divider border-color="#4f92ff" />
+          <mj-wrapper padding-top="0">
+            <mj-section padding-top="0">
+              <mj-column>
+                <mj-text>
+                  You are receiving this because you (or someone else) created a account ${process.env.SITE_TITLE}.
+                </mj-text>
+              </mj-column>
+            </mj-section>
+            <mj-section>
+              <mj-column>
+                <mj-text>Please click activate to finalize your account creation.</mj-text>
+                <mj-text>If the link below does not work you can always go to <mj-raw><a href="http://${req.headers.host}/user/activation">http://${req.headers.host}/user/activation</a></mj-raw> and paste this code: ${token}</mj-text>
+                <mj-text>If you did not request this account to be made or want your data removed. Please click the delete button.</mj-text>
+              </mj-column>
+            </mj-section>
+            <mj-section>
+              <mj-column>
+                <mj-button href="http://${req.headers.host}/user/activation/${token}" font-family="Helvetica" background-color="#4f92ff" color="white">
+                  Activate
+                </mj-button>
+              </mj-column>
+              <mj-column>
+                <mj-button href="http://${req.headers.host}/user/delete" font-family="Helvetica" background-color="#4f92ff" color="white">
+                  Delete Account
+                </mj-button>
+              </mj-column>
+            </mj-section>
+          </mj-wrapper>
+        </mj-body>
+      </mjml>`)
+          var accountActvationEmail = {
+            to: req.body.email,
+            from: `${process.env.SITE_TITLE} No-Reply <noreply@${process.env.EMAIL_DOMAIN}>`,
+            subject: `Account actvation  | ${process.env.SITE_TITLE}`,
+            html: htmlOuput.html
+          };
+          nodemailerSendGrid.sendMail(accountActvationEmail, function (err) {
+            req.flash('success', success)
+            res.redirect('/login');
+            done(err, 'done');
+          })
+        }
+      ])
+    });
+  }
 });
 
 module.exports = router;
