5529d5662182cf699413857f2122e8fe9512675e
Nathan Henniges <demonwolf@demonwolfdev.com>
2019-11-20 19:40:21 -0600

    Made channges.

--- src/config/sendgrid.js
+++
@@ -1,9 +0,0 @@
-const nodemailer = require('nodemailer');
-const nodemailerSendgrid = require('nodemailer-sendgrid');
-
-module.exports =  nodemailer.createTransport(
-  nodemailerSendgrid({
-    apiKey: process.env.SENDGRID_API_KEY
-  })
-);
-
