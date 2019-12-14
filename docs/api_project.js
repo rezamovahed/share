4667dec7150bb723c8a54a43224ee3b6d1131d1e
Nathan Henniges <demonwolf@demonwolfdev.com>
2019-12-14 11:14:16 -0600

    small api doc tweaks

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,16 +1,16 @@
 define({
   "name": "Share",
   "version": "4.0.0",
-  "description": "Advanced uploader with web front-end for images,files,and text. Built with ShareX in mind. Licensed under MIT and is free to use.",
+  "description": "Advanced custom uploader with web front-end.  Allows uploads for  images, files, and text. Built with ShareX in mind.",
   "title": "Share | Made by MrDemonWolf",
-  "url": "https://www.example.com/api",
+  "url": "https://www.example.com/",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-11-30T21:40:32.651Z",
+    "time": "2019-12-14T17:13:18.233Z",
     "url": "http://apidocjs.com",
-    "version": "0.18.0"
+    "version": "0.19.0"
   }
 });
