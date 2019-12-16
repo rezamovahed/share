22dc3cdd2840f4848b27ab01bed3abd7e32fec30
Nathan Henniges <demonwolf@demonwolfdev.com>
2019-12-15 19:22:50 -0600

    Edited test and added one for forgot-password.

--- test/api.spec.js
+++ test/api.spec.js
@@ -4,10 +4,10 @@ const supertest = require('supertest');
 
 const app = require('../src/index');
 
-describe('GET /api', () => {
+describe('GET /api/v1', () => {
   it('it should has status code 200', done => {
     supertest(app)
-      .get('/api')
+      .get('/api/v1')
       .expect(200)
       .end((err, res) => {
         if (err) done(err);
