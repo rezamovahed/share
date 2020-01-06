4e543cd72ea70af8fd69a922ceeb46a3fc202c59
Nathan Henniges <demonwolf@demonwolfdev.com>
2020-01-06 13:31:04 -0600

    Fixed the year in the readme.

--- src/controllers/api/upload.js
+++ src/controllers/api/upload.js
@@ -3,16 +3,36 @@ const alphabet =
 const generate = require('nanoid/generate');
 const fs = require('fs-extra');
 const path = require('path');
+const filesize = require('filesize');
 
+/**
+ * Load MongoDB models.
+ */
+const Upload = require('../../models/Upload');
+
+/**
+ * Upload file Controler - Allows users upload a file or image using there API Key
+ * This returns the URL where the file is hosted and a delete link.
+ */
 module.exports.uploadFile = async (req, res, next) => {
   const { file } = req.files;
   const fileExtension = path.extname(file.name);
   const fileMineType = file.mimetype;
-  const newFileName = generate(alphabet, 24) + fileExtension;
-  const uploadPath = `${path.join(
-    __dirname,
-    '../../public'
-  )}/u/i/${newFileName}`;
-  // Delete key is for the delete URL via the get request
-  const deleteKey = generate(alphabet, 24);
+  const fileName = generate(alphabet, 24);
+  const fileNameWithExt = fileName + fileExtension;
+  const uploadPath = `${path.join(__dirname, '../../public')}/u/i/${fileName}`;
+  // Delete key is fileName the delete URL via the get request
+  const deleteKey = generate(alphabet, 32);
+
+  const size = filesize(file.size);
+
+  const upload = new Upload({
+    uploader: req.user.id,
+    fileName,
+    fileExtension,
+    deleteKey,
+    size
+  });
+
+  res.json(upload);
 };
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -16,4 +16,46 @@ router.get('/', (req, res) => {
   });
 });
 
+/**
+ * @route /admin/uploads
+ * @method GET
+ * @description Displays all the uploads from all users.
+ * @access Private
+ */
+router.get('/uploads', (req, res) => {
+  res.render('admin/uploads', {
+    pageTitle: 'Uploads',
+    pageDesc: process.env.DESC,
+    pageName: 'adminUploads'
+  });
+});
+
+/**
+ * @route /admin/gallery
+ * @method GET
+ * @description Displays all the uploads from all users in gallery formate
+ * @access Private
+ */
+router.get('/gallery', (req, res) => {
+  res.render('admin/gallery', {
+    pageTitle: 'Gallery',
+    pageDesc: process.env.DESC,
+    pageName: 'adminGallery'
+  });
+});
+
+/**
+ * @route /admin
+ * @method GET
+ * @description Displays a admin dashboard
+ * @access Private
+ */
+router.get('/users', (req, res) => {
+  res.render('admin/users/index', {
+    pageTitle: 'Users',
+    pageDesc: process.env.DESC,
+    pageName: 'adminUsers'
+  });
+});
+
 module.exports = router;
