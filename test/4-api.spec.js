a5a74d83c0151950caa1c0c621f65fabbff80360
Nathan Henniges <demonwolf@demonwolfdev.com>
2020-02-02 12:57:18 -0600

    Added tests and fixed the naming.

--- test/4-api.spec.js
+++ test/4-api.spec.js
@@ -4,26 +4,29 @@ const supertest = require('supertest');
 
 const app = require('../src/index');
 
-describe('GET /api', () => {
-  it('it should has status code 200', done => {
-    supertest(app)
-      .get('/api')
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        done();
-      });
+describe('API Routes', () => {
+  describe('GET /api', () => {
+    it('it should has status code 200', done => {
+      supertest(app)
+        .get('/api')
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+          done();
+        });
+    });
   });
-});
-
-describe('GET /api/v1', () => {
-  it('it should has status code 200', done => {
-    supertest(app)
-      .get('/api/v1')
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        done();
+  describe('API Version 1', () => {
+    describe('GET /api/v1', () => {
+      it('it should has status code 200', done => {
+        supertest(app)
+          .get('/api/v1')
+          .expect(200)
+          .end((err, res) => {
+            if (err) return done(err);
+            done();
+          });
       });
+    });
   });
 });
