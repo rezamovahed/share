0175101a0fb665e2083e97bbda7a801aee0b1438
Nathan Henniges <demonwolf@demonwolfdev.com>
2020-02-09 19:28:54 -0600

    fixing travis3.

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -24,14 +24,16 @@ exports.postSignup = async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
+    const slug = slugify(username, {
+      remove: /[*+~.()'"!:@]/g,
+      lower: true
+    });
+
     const user = new User({
       username,
       email,
       password,
-      slug: slugify(username, {
-        remove: /[*+~.()'"!:@]/g,
-        lower: true
-      })
+      slug
     });
 
     // Set the token and the expire date.
