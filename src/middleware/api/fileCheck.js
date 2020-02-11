a50f980b4354d44e719654123daff965db875354
Nathan Henniges <demonwolf@demonwolfdev.com>
2020-02-11 17:15:30 -0600

    Now checks uploaded files if the fileCheck is enabled.

--- src/middleware/api/fileCheck.js
+++ src/middleware/api/fileCheck.js
@@ -2,7 +2,19 @@ const mineTypes = require('../../config/mineTypes');
 
 module.exports = async (req, res, next) => {
   if (process.env.FILE_CHECK) {
+    const allowedMineTypes = mineTypes.files.concat(
+      mineTypes.images.concat(mineTypes.text)
+    );
+    const isAllowedFile = allowedMineTypes.includes(req.files.file.mimetype);
+    if (!isAllowedFile) {
+      return res.status(400).json({
+        auth: true,
+        success: false,
+        error: {
+          message: 'Uploaded file is not allowed.'
+        }
+      });
+    }
   }
-
   next();
 };
