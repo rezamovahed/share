0bf46ba51657fe012128d938ce0ee057ee730ec2
Nathan Henniges <demonwolf@demonwolfdev.com>
2020-04-11 17:25:28 -0500

    Moved the dev text right next to the version

--- src/index.js
+++ src/index.js
@@ -170,7 +170,7 @@ app.use(async (req, res, next) => {
   res.locals.signupTerms = process.env.SIGNUP_TERMS === 'true';
   res.locals.version =
     process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
-      ? `${process.env.npm_package_version} dev`
+      ? `${process.env.npm_package_version}dev`
       : process.env.npm_package_version;
   // Pass flash to locals
   res.locals.info = req.flash('info');
