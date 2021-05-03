94afb487dec76a3c2277744a5bef4e606e534414
Nathan Henniges <demonwolf@demonwolfdev.com>
2021-05-03 12:04:31 -0500

    [SHARE-55] Added create APIKey route. (#280)

--- server/routes/apiKey.js
+++ server/routes/apiKey.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const moment = require('moment');
+const sha512 = require('js-sha512');
 
 const router = express.Router();
 
@@ -36,6 +39,75 @@ router.get('/', requireAuth, isSessionValid, async (req, res) => {
     const totalAPIKeys = apikeys.length;
 
     res.status(200).json({ apikeys, total: totalAPIKeys });
+  } catch (e) {
+    res.status(500).json({
+      code: 'INTERNAL_SERVER_ERROR',
+      error: 'Internal Server Error.'
+    });
+  }
+});
+
+/**
+ * @route /apikey
+ * @method POST
+ * @description Allows a logged in user to create a API Key
+ */
+router.post('/', requireAuth, isSessionValid, async (req, res) => {
+  try {
+    /**
+     * Create the JWT payload
+     */
+    const payload = {
+      sub: req.user.id,
+      iss: `${process.env.SITE_TITLE} 3rd-party API`
+    };
+
+    const { label, expires } = req.body;
+
+    // Setup variables to use for the switch case statement
+    // This is so it can know what date the token should expire and or if never.
+    let expireAt;
+    let expiresIn;
+    let isNever;
+
+    switch (expires) {
+      case 'day':
+        expireAt = moment().add('24', 'h');
+        expiresIn = '24h';
+        break;
+      case 'week':
+        expireAt = moment().add('7', 'd');
+        expiresIn = '7d';
+        break;
+      case 'month':
+        expireAt = moment().add('1', 'M');
+        expiresIn = '31d';
+        break;
+      default:
+        expireAt = moment().add('100', 'y');
+        expiresIn = '36500d';
+        isNever = true;
+    }
+
+    const apiKey = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn
+    });
+
+    const apiKeyHash = sha512(apiKey);
+
+    await APIKey.create({
+      user: req.user.id,
+      label,
+      hash: apiKeyHash,
+      expireAt,
+      isNever
+    });
+
+    res.status(201).json({
+      code: 'ADDED',
+      message: 'Added new API Key',
+      api_key: apiKey
+    });
   } catch (e) {
     res.status(500).json({
       code: 'INTERNAL_SERVER_ERROR',
